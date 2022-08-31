f6eb440df35c7d0a12909b3e8d43e8cc8a3c9405
Agustin Levental <agustinlevental@gmail.com>
2022-08-31 10:21:21 -0300

    Products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Basket from './components/Basket';
+import data from './data';
 
 function App() {
+  const {products}=data;
   return (
     <div className="App">
      <Header></Header>
-     <div>
-     <Main></Main>
+     <div className='row'>
+     <Main products={products}></Main>
      <Basket></Basket>
      </div>
     </div>
